35f5ef56ee9c1ea1235e8c634d332d3e7d75ce8b
Artem Bespolov <abespolov@gmail.com>
2019-04-04 13:41:28 +0300

    change server path

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get("/ping", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "build")));
 
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+        res.sendFile(path.join(__dirname, "build", "index.html"));
     });
 }
 app.listen(port, (req, res) => {
