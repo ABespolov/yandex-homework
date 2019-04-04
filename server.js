56d6c9e82f794c2b1ed22c8e5f537429c1822b1e
Artem Bespolov <abespolov@gmail.com>
2019-04-04 14:12:11 +0300

    edit server

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ app.get("/ping", (req, res) => {
     res.send("pong");
 });
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "build")));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "build", "index.html"));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
+
 app.listen(port, (req, res) => {
     // console.log(`server listening on port: ${port}`);
 });
