5da72ac36f1a28d34cfb838b2956484236070a7f
Artem Bespolov <abespolov@gmail.com>
2019-04-04 14:36:19 +0300

    try to deploy

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const path = require("path");
 const port = process.env.PORT || 8000;
 const app = express();
 
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    //
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    })
+}
+//build mode
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
+
 app.get("/ping", (req, res) => {
     res.send("pong");
 });
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
 
 app.listen(port, (req, res) => {
     // console.log(`server listening on port: ${port}`);
